7917c3e49164dc1560d12f5377274c8517d33b04
Marcel Feliu Saperas <marcel.feliu@gracia.lasalle.cat>
2026-02-07 17:47:38 +0100

    style: create color variables

--- app/src/main/java/com/example/marcelfeliu_pausole_projecte/ui/theme/Color.kt
+++ app/src/main/java/com/example/marcelfeliu_pausole_projecte/ui/theme/Color.kt
@@ -9,3 +9,10 @@ val Pink80 = Color(0xFFEFB8C8)
 val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
+
+val RM_BarBg = Color(0xFFE6F7F2)
+val RM_Green = Color(0xFF00FF9C)
+val RM_Text = Color(0xFF1F2A32)
+
+
+

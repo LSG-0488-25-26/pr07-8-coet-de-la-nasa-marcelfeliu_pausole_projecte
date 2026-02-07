8ee856de38d58faa611aab694fa5df8dd32a9e70
Marcel Feliu Saperas <marcel.feliu@gracia.lasalle.cat>
2026-02-07 17:48:20 +0100

    style: update detail screen

--- app/src/main/java/com/example/marcelfeliu_pausole_projecte/view/DetailScreen.kt
+++ app/src/main/java/com/example/marcelfeliu_pausole_projecte/view/DetailScreen.kt
@@ -5,6 +5,7 @@ import android.content.res.Configuration
 import android.view.WindowManager
 import android.widget.ScrollView
 import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.gestures.scrollable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
@@ -19,8 +20,10 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
@@ -28,6 +31,7 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.ColorFilter
 import androidx.compose.ui.graphics.ColorMatrix
@@ -62,9 +66,9 @@ fun DetailScreen(navController: NavController, viewModel: RickAndMortyViewModel,
     Box(
         modifier = modifier
             .fillMaxSize()
+            .background(Color(0xFF0E1B1F))
             .verticalScroll(rememberScrollState()),
         contentAlignment = Alignment.Center
-
     ) {
         if (configuration.orientation == Configuration.ORIENTATION_PORTRAIT) {
             Column(
@@ -75,26 +79,26 @@ fun DetailScreen(navController: NavController, viewModel: RickAndMortyViewModel,
                     GlideImage(
                         model = character!!.image,
                         contentDescription = character!!.name,
-                        colorFilter = ColorFilter.colorMatrix(ColorMatrix().apply
-                        {
-                            if (isCaptured) {
-                                setToSaturation(0f)
-                            } else {
-                                setToSaturation(1f)
+                        colorFilter = ColorFilter.colorMatrix(
+                            ColorMatrix().apply {
+                                if (isCaptured) setToSaturation(0f) else setToSaturation(1f)
                             }
-                        }),
+                        ),
                         contentScale = ContentScale.Crop,
                         modifier = Modifier
                             .size(320.dp)
-
+                            .clip(RoundedCornerShape(20.dp))
+                            .border(3.dp, Color(0xFF00FF9C), RoundedCornerShape(20.dp))
                     )
+
 
                     Spacer(modifier = Modifier.height(10.dp))
 
                     Text(
                         text = character!!.name,
-                        fontSize = 28.sp,
-                        fontWeight = FontWeight.Bold
+                        fontSize = 30.sp,
+                        fontWeight = FontWeight.Bold,
+                        color = Color(0xFF00FF9C)
                     )
 
                     Spacer(modifier = Modifier.height(8.dp))
@@ -103,36 +107,48 @@ fun DetailScreen(navController: NavController, viewModel: RickAndMortyViewModel,
                         text = "Specie: ${character!!.species.lowercase().replaceFirstChar { it.uppercase() }}",
                         fontSize = 20.sp,
                         fontWeight = FontWeight.Medium,
+                        color = Color(0xFFE6F7F2)
+
                     )
 
                     Text(
                         text = "Status: ${character!!.status.lowercase().replaceFirstChar { it.uppercase() }}",
                         fontSize = 20.sp,
                         fontWeight = FontWeight.Medium,
+                        color = Color(0xFFE6F7F2)
+
                     )
 
                     Text(
                         text = "Gender: ${character!!.gender.lowercase().replaceFirstChar { it.uppercase() }}",
                         fontSize = 20.sp,
                         fontWeight = FontWeight.Medium,
+                        color = Color(0xFFE6F7F2)
+
                     )
 
                     Text(
                         text = "Location: ${character!!.location.name.lowercase().replaceFirstChar { it.uppercase() }}",
                         fontSize = 20.sp,
                         fontWeight = FontWeight.Medium,
+                        color = Color(0xFFE6F7F2)
+
                     )
 
                     Text(
                         text = "Origin: ${character!!.origin.name.lowercase().replaceFirstChar { it.uppercase() }}",
                         fontSize = 20.sp,
                         fontWeight = FontWeight.Medium,
+                        color = Color(0xFFE6F7F2)
+
                     )
 
                     Text(
-                        text = "N of episodes: ${character!!.episode.count()}",
+                        text = "Nº of episodes: ${character!!.episode.count()}",
                         fontSize = 20.sp,
                         fontWeight = FontWeight.Medium,
+                        color = Color(0xFFE6F7F2)
+
                     )
 
 
@@ -159,23 +175,31 @@ fun DetailScreen(navController: NavController, viewModel: RickAndMortyViewModel,
                                     viewModel.toggleIsCapturingCharacter()
                                 }
                             }
-                        }
+                        },
+                        colors = ButtonDefaults.buttonColors(
+                            containerColor = Color(0xFF00FF9C),
+                            contentColor = Color.Black
+                        )
                     ) {
-                        if (isCaptured){
-                            Text("Free")
-                        } else {
-                            Text("Capture")
-                        }
+                        Text(if (isCaptured) "Free" else "Capture")
                     }
+
                     Spacer(modifier = Modifier.width(20.dp))
                     Button(
-                        onClick =
-                            {
-                                navController.popBackStack()
-                            }
+                        onClick = { navController.popBackStack() },
+                        colors = ButtonDefaults.buttonColors(
+                            containerColor = Color(0xFF1F2A32),
+                            contentColor = Color(0xFF00FF9C)
+                        ),
+                        modifier = Modifier.border(
+                            1.5.dp,
+                            Color(0xFF00FF9C),
+                            RoundedCornerShape(50)
+                        )
                     ) {
                         Text("Go Back")
                     }
+
                 }
             }
         } else {
